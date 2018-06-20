f54ccf7605932a85a32ad2af57408a7185fc5b79
Yudovich <yudovichml@gmail.com>
2018-06-20 09:53:47 +0300

    fixing self stupidity

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	var paramString = windows.location.href.split("?")[1];
+	var paramString = window.location.href.split("?")[1];
 	var searchParams = new URLSearchParams(paramString);
 	if (!$.cookies("vk_auth_token")) {
 		if (!searchParams.has("access_token")) {
