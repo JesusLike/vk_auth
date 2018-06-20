1c97f7e3bb88c32baaf5664513abf9f08cf69103
Yudovich <yudovichml@gmail.com>
2018-06-20 03:47:42 +0300

    request fix

--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,13 @@
 function vk_auth() {
-	var jqxhr = $.get("https://oauth.vk.com/authorize\
-		?client_id=6610605\
-		&redirect_uri=https://jesuslike.github.io/vk_auth\
-		&display=popup\
-		&scope=2\
-		&response_type=token\
-		&v=5.80");
+	var jqxhr = $.ajax({
+		url: "https://oauth.vk.com/authorize\
+			?client_id=6610605\
+			&redirect_uri=https://jesuslike.github.io/vk_auth\
+			&display=popup\
+			&scope=2\
+			&response_type=token\
+			&v=5.80",
+		type: "GET",
+		dataType: "jsonp",
+		crossDomain: "true");
 }
