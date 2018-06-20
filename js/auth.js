e9dee3e0c0e7cdc6aeee45e99ec5c0e0ea205117
Yudovich <yudovichml@gmail.com>
2018-06-20 04:01:06 +0300

    fixed broken url

--- js/auth.js
+++ js/auth.js
@@ -1,12 +1,12 @@
 function vk_auth() {
 	var jqxhr = $.ajax({
-		url: "https://oauth.vk.com/authorize\
-			?client_id=6610605\
-			&redirect_uri=https://jesuslike.github.io/vk_auth\
-			&display=popup\
-			&scope=2\
-			&response_type=token\
-			&v=5.80",
+		url: "https://oauth.vk.com/authorize" +
+			"?client_id=6610605" +
+			"&display=popup" +
+			"&redirect_uri=https://jesuslike.github.io/vk_auth" +
+			"&scope=2" +
+			"&response_type=token" +
+			"&v=5.80",
 		type: "GET",
 		dataType: "jsonp",
 		crossDomain: "true"
