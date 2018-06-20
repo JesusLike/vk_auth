9a73e5d05fbcaa62776a39cad1001e25bc800410
Yudovich <yudovichml@gmail.com>
2018-06-20 10:12:51 +0300

    fixed cross-domain request

--- js/get-info.js
+++ js/get-info.js
@@ -1,11 +1,29 @@
 function getInfo(auth_token) {
-	var userInfo = $.get("https://api.vk.com/method/users.get?fields=nickname&access_token=" + auth_token + "&v=5.80").response[0];
-	var friendList = $.get("https://api.vk.com/method/friends.get?order=random&count=5&fields=nickname&access_token=" +
-		+ auth_token).response.items.map(function(item) {
-			return item.first_name + " " + item.last_name;
-		});
+	var username;
+	var friendList;
+
+	$.ajax({
+		url: "https://api.vk.com/method/users.get?fields=nickname&access_token=" + auth_token + "&v=5.80",
+		dataType: "jsonp",
+		crossDomain: true,
+		success: function(data) {
+			username = data.response[0].first_name + " " + data.response[0].last_name;
+		}
+	});
+	
+	$.ajax({
+		url: "https://api.vk.com/method/friends.get?order=random&count=5&fields=nickname&access_token=" + auth_token,
+		dataType: "jsonp",
+		crossDomain: true,
+		success: function(data) {
+			friendList = data.response.items.map(function(item) {
+				return item.first_name + " " + item.last_name;
+			});
+		}
+	});
+
 	return {
-		username: userInfo.first_name + " " + userInfo.last_name,
+		username: username,
 		friends: friendList
 	};
 }
