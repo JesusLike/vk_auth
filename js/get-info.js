4729cb9cbf3ad41c5e7305baa8320658ea7870b1
Yudovich <yudovichml@gmail.com>
2018-06-20 18:45:07 +0300

    some markup changes

--- js/get-info.js
+++ js/get-info.js
@@ -8,14 +8,15 @@ function getInfo(auth_token) {
 			$("#username").text(data.response[0].first_name + " " + data.response[0].last_name);
 			$.ajax({
 				method: "GET",
-				url: "https://api.vk.com/method/friends.get?order=random&count=5&fields=nickname&access_token=" + auth_token + "&v=5.80",
+				url: "https://api.vk.com/method/friends.get?order=random&count=5&fields=nickname&access_token=" + 
+					auth_token + "&v=5.80",
 				dataType: "jsonp",
 				crossDomain: true,
 				success: function(data) {
 					data.response.items.map(function(item) {
 						return item.first_name + " " + item.last_name;
 					}).forEach(function(item) {
-						$("#friends").append(item + "<br>");
+						$("#friends ul").append("<li>" + item + "</li>");
 					});
 				}
 			});
--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
 	var searchParams = new URLSearchParams(paramString);
 	if (!$.cookie("vk_auth_token")) {
 		if (!searchParams.has("access_token")) {
-			$("#auth-button").attr("value", "Log In");
+			$("#auth-button").attr("value", "Авторизоваться");
 			$("#auth-button").click(vk_auth);
 			return;		
 		} else {
 			$.cookie("vk_auth_token", searchParams.get("access_token"), parseInt(searchParams.get("expires_in")) / 60 / 60 / 24);
 		}
 	}
-	$("#auth-button").attr("value", "Log Out");
+	$("#auth-button").attr("value", "Выйти");
 	getInfo($.cookie("vk_auth_token"));
 });
