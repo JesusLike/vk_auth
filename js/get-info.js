34dd026c37a27eec4cdfca34b847176aedf5c964
Yudovich <yudovichml@gmail.com>
2018-06-20 18:36:05 +0300

    fixed async bug

--- js/get-info.js
+++ js/get-info.js
@@ -1,40 +1,24 @@
 function getInfo(auth_token) {
-	var username;
-	var friendList;
-
 	$.ajax({
 		method: "GET",
 		url: "https://api.vk.com/method/users.get?fields=nickname&access_token=" + auth_token + "&v=5.80",
 		dataType: "jsonp",
 		crossDomain: true,
 		success: function(data) {
-			alert(5);
-			username = data.response[0].first_name + " " + data.response[0].last_name;
-		}
-	});
-	
-	$.ajax({
-		method: "GET",
-		url: "https://api.vk.com/method/friends.get?order=random&count=5&fields=nickname&access_token=" + auth_token + "&v=5.80",
-		dataType: "jsonp",
-		crossDomain: true,
-		success: function(data) {
-			alert(5);
-			console.log(data);
-			console.log(data.response);
-			console.log(data.response.items);
-			friendList = data.response.items.map(function(item) {
-				return item.first_name + " " + item.last_name;
+			$("#username").text(data.response[0].first_name + " " + data.response[0].last_name);
+			$.ajax({
+				method: "GET",
+				url: "https://api.vk.com/method/friends.get?order=random&count=5&fields=nickname&access_token=" + auth_token + "&v=5.80",
+				dataType: "jsonp",
+				crossDomain: true,
+				success: function(data) {
+					data.response.items.map(function(item) {
+						return item.first_name + " " + item.last_name;
+					}).forEach(function(item) {
+						$("#friends").append(item + "<br>");
+					});
+				}
 			});
 		}
 	});
-
-	console.log(username);
-	console.log(friendList);
-	console.log("Get info done");
-
-	return {
-		username: username,
-		friends: friendList
-	};
 }
