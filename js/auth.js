352af454fba855837cbe7a50a7043ab9d7650d29
Yudovich <yudovichml@gmail.com>
2018-06-20 03:37:00 +0300

    oauth request

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,9 @@
 function vk_auth() {
-	var jqxhr = $.get("google.com");
+	var jqxhr = $.get("https://oauth.vk.com/authorize\
+		?client_id=6610605\
+		&redirect_uri=https://jesuslike.github.io/vk_auth\
+		&display=popup\
+		&scope=2\
+		&response_type=token\
+		&v=5.80");
 }
