b3b3fe45350c6108101b7936c1297f39dde6faae
Yudovich <yudovichml@gmail.com>
2018-06-20 09:56:14 +0300

    and one more time

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 	var paramString = window.location.href.split("?")[1];
 	var searchParams = new URLSearchParams(paramString);
-	if (!$.cookies("vk_auth_token")) {
+	if (!$.cookie("vk_auth_token")) {
 		if (!searchParams.has("access_token")) {
 			$("#auth-button").attr("value", "Log In");
 			$("#auth-button").click(vk_auth);
 			return;		
 		} else {
-			$.cookies("vk_auth_token", searchParams.get("access_token"), parseInt(searchParams.get("expires_in")) / 60 / 60 / 24);
+			$.cookie("vk_auth_token", searchParams.get("access_token"), parseInt(searchParams.get("expires_in")) / 60 / 60 / 24);
 		}
 	}
 	$("#auth-button").attr("value", "Log Out");
-	var output = getInfo($.cookies("vk_auth_token"));
+	var output = getInfo($.cookie("vk_auth_token"));
 	$("#username").text(output.username);
 	output.friends.forEach(function(item) {
 		$("#friends").append(item + "<br>");
